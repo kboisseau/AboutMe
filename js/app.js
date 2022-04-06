ebc50c63fb8618c4094f6df09125198bb3dea753
Kyel <kboisseau93@gmail.com>
2022-04-06 15:57:19 -0700

    Changes made to lab 2

---
+++ js/app.js
@@ -0,0 +1,73 @@
+'use strict';
+
+
+
+let siteVistor = prompt('What is your name?');
+let loopcheck = false;
+
+alert(`Welcome, ${siteVistor}! I would like to play a game. Please answer "Yes" or "No".`)
+
+while (loopCheck !== true ) {
+  let responeOne= prompt('I drive a wrx. "Yes or No"?') .toLowerCase();
+
+  if (responseOne === 'yes' || responseOne === 'y') {
+     // console.log('Great answer!');
+     alert('Great answer!');
+     loopcheck = true;
+  } else if( responseOne === 'no' || responseOne === 'n'){
+    // console.log('Sorry, that is incorrect.');
+    alert('Sorry, that is incorrect.');
+    loopcheck = true;
+  }else if
+
+
+
+  while (loopCheck !== true ) {
+    let responeTwo= prompt('Did i break my arm falling in a ditch? "Yes or No"?') .toLowerCase();
+  
+    if (responseTwo === 'yes' || responseTwo === 'y') {
+       // console.log('Great answer!');
+       alert('Great answer!');
+       loopcheck = true;
+    } else if( responseTwo === 'no' || responseTwo === 'n'){
+      // console.log('Sorry, that is incorrect.');
+      alert('Sorry, that is incorrect.');
+      loopcheck = true;
+    }else if
+  
+  
+
+
+    while (loopCheck !== true ) {
+      let responeThree= prompt('I speak french. "Yes or No"?') .toLowerCase();
+    
+      if (responseThree === 'yes' || responseThree === 'y') {
+         // console.log('Great answer!');
+         alert('Great answer!');
+         loopcheck = true;
+      } else if( responseThree === 'no' || responseThree === 'n'){
+        // console.log('Sorry, that is incorrect.');
+        alert('Sorry, that is incorrect.');
+        loopcheck = true;
+      }else if
+    
+    
+
+      while(loopCheck !== true ) {
+        let responeFour= prompt('I recently moved to Washington. "Yes or No"?') .toLowerCase();
+      
+        if (responseFour === 'yes' || responseFour === 'y') {
+           // console.log('Great answer!');
+           alert('Great answer!');
+           loopcheck = true;
+        } else if( responseFour === 'no' || responseFour === 'n'){
+          // console.log('Sorry, that is incorrect.');
+          alert('Sorry, that is incorrect.');
+          loopcheck = true;
+        }else if
+      
+      
+
+
+  
+}
