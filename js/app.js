46175aab7a790c15dd9e02ab4c562387e1243a8c
Guy Farley <guy.e.farley@gmail.com>
2022-04-07 13:39:17 -0700

    Corrects while loop

--- js/app.js
+++ js/app.js
@@ -7,65 +7,65 @@ let loopCheck = false;
 
 alert(`Welcome, ${siteVistor}! I would like to play a game. Please answer "Yes" or "No".`);
 
-while (loopCheck !== true) {
-  let responseOne = prompt('I drive a wrx. "Yes or No"?').toLowerCase();
+// while (loopCheck !== true) {
+let responseOne = prompt('I drive a wrx. "Yes or No"?').toLowerCase();
 
-  if (responseOne === 'yes' || responseOne === 'y') {
-    // console.log('Great answer!');
-    alert('Great answer!');
-    loopCheck = true;
-  } else if (responseOne === 'no' || responseOne === 'n') {
-    // console.log('Sorry, that is incorrect.');
-    alert('Sorry, that is incorrect.');
-    loopCheck = true;
-  }
+if (responseOne === 'yes' || responseOne === 'y') {
+  // console.log('Great answer!');
+  alert('Great answer!');
+  loopCheck = true;
+} else if (responseOne === 'no' || responseOne === 'n') {
+  // console.log('Sorry, that is incorrect.');
+  alert('Sorry, that is incorrect.');
+  loopCheck = true;
 }
+// }
 
 
-while (loopCheck !== true) {
-  let responseTwo = prompt('Did i break my arm falling in a ditch? "Yes or No"?').toLowerCase();
+// while (loopCheck !== true) {
+let responseTwo = prompt('Did i break my arm falling in a ditch? "Yes or No"?').toLowerCase();
 
-  if (responseTwo === 'yes' || responseTwo === 'y') {
-    // console.log('Great answer!');
-    alert('Great answer!');
-    loopCheck = true;
-  } else if (responseTwo === 'no' || responseTwo === 'n') {
-    // console.log('Sorry, that is incorrect.');
-    alert('Sorry, that is incorrect.');
-    loopCheck = true;
-  }
+if (responseTwo === 'yes' || responseTwo === 'y') {
+  // console.log('Great answer!');
+  alert('Great answer!');
+  loopCheck = true;
+} else if (responseTwo === 'no' || responseTwo === 'n') {
+  // console.log('Sorry, that is incorrect.');
+  alert('Sorry, that is incorrect.');
+  loopCheck = true;
 }
+// }
 
 
 
-while (loopCheck !== true) {
-  let responseThree = prompt('I speak french. "Yes or No"?').toLowerCase();
+// while (loopCheck !== true) {
+let responseThree = prompt('I speak french. "Yes or No"?').toLowerCase();
 
-  if (responseThree === 'yes' || responseThree === 'y') {
-    // console.log('Great answer!');
-    alert('Great answer!');
-    loopCheck = true;
-  } else if (responseThree === 'no' || responseThree === 'n') {
-    // console.log('Sorry, that is incorrect.');
-    alert('Sorry, that is incorrect.');
-    loopCheck = true;
-  }
+if (responseThree === 'yes' || responseThree === 'y') {
+  // console.log('Great answer!');
+  alert('Great answer!');
+  loopCheck = true;
+} else if (responseThree === 'no' || responseThree === 'n') {
+  // console.log('Sorry, that is incorrect.');
+  alert('Sorry, that is incorrect.');
+  loopCheck = true;
 }
+// }
 
 
-while (loopCheck !== true) {
-  let responseFour = prompt('I recently moved to Washington. "Yes or No"?').toLowerCase();
+// while (loopCheck !== true) {
+let responseFour = prompt('I recently moved to Washington. "Yes or No"?').toLowerCase();
 
-  if (responseFour === 'yes' || responseFour === 'y') {
-    // console.log('Great answer!');
-    alert('Great answer!');
-    loopCheck = true;
-  } else if (responseFour === 'no' || responseFour === 'n') {
-    // console.log('Sorry, that is incorrect.');
-    alert('Sorry, that is incorrect.');
-    loopCheck = true;
-  }
+if (responseFour === 'yes' || responseFour === 'y') {
+  // console.log('Great answer!');
+  alert('Great answer!');
+  loopCheck = true;
+} else if (responseFour === 'no' || responseFour === 'n') {
+  // console.log('Sorry, that is incorrect.');
+  alert('Sorry, that is incorrect.');
+  loopCheck = true;
 }
+// }
 
 
 
