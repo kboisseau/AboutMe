f8062e1857e73ccb49304a32385de8b0a93458b5
Kyel <kboisseau93@gmail.com>
2022-04-07 13:19:42 -0700

    Changes to app.js

--- js/app.js
+++ js/app.js
@@ -3,71 +3,70 @@
 
 
 let siteVistor = prompt('What is your name?');
-let loopcheck = false;
+let loopCheck = false;
 
-alert(`Welcome, ${siteVistor}! I would like to play a game. Please answer "Yes" or "No".`)
+alert(`Welcome, ${siteVistor}! I would like to play a game. Please answer "Yes" or "No".`);
 
-while (loopCheck !== true ) {
-  let responeOne= prompt('I drive a wrx. "Yes or No"?') .toLowerCase();
+while (loopCheck !== true) {
+  let responseOne = prompt('I drive a wrx. "Yes or No"?').toLowerCase();
 
   if (responseOne === 'yes' || responseOne === 'y') {
-     // console.log('Great answer!');
-     alert('Great answer!');
-     loopcheck = true;
-  } else if( responseOne === 'no' || responseOne === 'n'){
+    // console.log('Great answer!');
+    alert('Great answer!');
+    loopCheck = true;
+  } else if (responseOne === 'no' || responseOne === 'n') {
     // console.log('Sorry, that is incorrect.');
     alert('Sorry, that is incorrect.');
-    loopcheck = true;
-  }else if
-
-
-
-  while (loopCheck !== true ) {
-    let responeTwo= prompt('Did i break my arm falling in a ditch? "Yes or No"?') .toLowerCase();
-  
-    if (responseTwo === 'yes' || responseTwo === 'y') {
-       // console.log('Great answer!');
-       alert('Great answer!');
-       loopcheck = true;
-    } else if( responseTwo === 'no' || responseTwo === 'n'){
-      // console.log('Sorry, that is incorrect.');
-      alert('Sorry, that is incorrect.');
-      loopcheck = true;
-    }else if
-  
-  
-
-
-    while (loopCheck !== true ) {
-      let responeThree= prompt('I speak french. "Yes or No"?') .toLowerCase();
-    
-      if (responseThree === 'yes' || responseThree === 'y') {
-         // console.log('Great answer!');
-         alert('Great answer!');
-         loopcheck = true;
-      } else if( responseThree === 'no' || responseThree === 'n'){
-        // console.log('Sorry, that is incorrect.');
-        alert('Sorry, that is incorrect.');
-        loopcheck = true;
-      }else if
-    
-    
-
-      while(loopCheck !== true ) {
-        let responeFour= prompt('I recently moved to Washington. "Yes or No"?') .toLowerCase();
-      
-        if (responseFour === 'yes' || responseFour === 'y') {
-           // console.log('Great answer!');
-           alert('Great answer!');
-           loopcheck = true;
-        } else if( responseFour === 'no' || responseFour === 'n'){
-          // console.log('Sorry, that is incorrect.');
-          alert('Sorry, that is incorrect.');
-          loopcheck = true;
-        }else if
-      
-      
-
-
-  
+    loopCheck = true;
+  }
 }
+
+
+while (loopCheck !== true) {
+  let responseTwo = prompt('Did i break my arm falling in a ditch? "Yes or No"?').toLowerCase();
+
+  if (responseTwo === 'yes' || responseTwo === 'y') {
+    // console.log('Great answer!');
+    alert('Great answer!');
+    loopCheck = true;
+  } else if (responseTwo === 'no' || responseTwo === 'n') {
+    // console.log('Sorry, that is incorrect.');
+    alert('Sorry, that is incorrect.');
+    loopCheck = true;
+  }
+}
+
+
+
+while (loopCheck !== true) {
+  let responseThree = prompt('I speak french. "Yes or No"?').toLowerCase();
+
+  if (responseThree === 'yes' || responseThree === 'y') {
+    // console.log('Great answer!');
+    alert('Great answer!');
+    loopCheck = true;
+  } else if (responseThree === 'no' || responseThree === 'n') {
+    // console.log('Sorry, that is incorrect.');
+    alert('Sorry, that is incorrect.');
+    loopCheck = true;
+  }
+}
+
+
+while (loopCheck !== true) {
+  let responseFour = prompt('I recently moved to Washington. "Yes or No"?').toLowerCase();
+
+  if (responseFour === 'yes' || responseFour === 'y') {
+    // console.log('Great answer!');
+    alert('Great answer!');
+    loopCheck = true;
+  } else if (responseFour === 'no' || responseFour === 'n') {
+    // console.log('Sorry, that is incorrect.');
+    alert('Sorry, that is incorrect.');
+    loopCheck = true;
+  }
+}
+
+
+
+
